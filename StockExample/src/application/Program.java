dc6f3fea37ffa971d88a920e83b3b72dd0bbbd00
Luisa Santos <luisacristina.sn@icloud.com>
2024-01-29 22:02:03 -0300

    toString

--- StockExample/src/application/Program.java
+++ StockExample/src/application/Program.java
@@ -17,6 +17,7 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
 
+        System.out.println(product);
 
 
 
--- StockExample/src/entities/Product.java
+++ StockExample/src/entities/Product.java
@@ -20,4 +20,15 @@ public class Product {
         this.quantity -= quantity;
 
     }
+
+    public String toString(){
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f",totalValueInStock());
+
+    }
 }
